a0fa264498d7c184fcfb5da919762f1f08f1936a
abhishek gupta <abhi19gupta@gmail.com>
2015-10-27 08:15:25 +0530

    bug resolved

--- 3/main_new.cpp
+++ 3/main_new.cpp
@@ -13,77 +13,10 @@
 
 using namespace cv;
 using namespace std;
-/*
-Mat energyIndivisual(Mat im, Mat seam, bool toDisplay){
-	Mat M(im.rows,im.cols, CV_32SC1);
-	int maxima = 0;
 
-	for (int i = 1; i < im.rows-1; ++i){
-		for (int j = 1; j < im.cols-1; ++j){
-			if(seam.at<char>(i,j)==255){
-				M.at<int>(i,j)=INT_MAX;
-				continue;
-			}
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
 
-			int nextUp = i-1;
-			while(seam.at<char>(nextUp,j)==255 && nextUp>=0){
-				nextUp--;
-			}
-			if(nextUp==-1){
-				M.at<int>(i,j)=INT_MAX;
-				continue;
-			}
-			int nextDown = i+1;
-			while(seam.at<char>(nextDown,j)==255 && nextDown<=im.rows-1){
-				nextDown++;
-			}
-			if(nextDown==im.rows){
-				M.at<int>(i,j)=INT_MAX;
-				continue;
-			}
-			int nextLeft = j-1;
-			while(seam.at<char>(i,nextLeft)==255 && nextLeft>=0){
-				nextLeft--;
-			}
-			if(nextLeft==-1){
-				M.at<int>(i,j)=INT_MAX;
-				continue;
-			}
-			int nextRight = j+1;
-			while(seam.at<char>(i,nextRight)==255 && nextRight<=im.cols-1){
-				nextRight++;
-			}
-			if(nextRight==im.cols){
-				M.at<int>(i,j)=INT_MAX;
-				continue;
-			}
-			int bi = im.at<Vec3b>(nextUp,j)[0] - im.at<Vec3b>(nextDown,j)[0];
-			int gi = im.at<Vec3b>(nextUp,j)[1] - im.at<Vec3b>(nextDown,j)[1];
-			int ri = im.at<Vec3b>(nextUp,j)[2] - im.at<Vec3b>(nextDown,j)[2];
-			int bj = im.at<Vec3b>(i,nextLeft)[0] - im.at<Vec3b>(i,nextRight)[0];
-			int gj = im.at<Vec3b>(i,nextLeft)[1] - im.at<Vec3b>(i,nextRight)[1];
-			int rj = im.at<Vec3b>(i,nextLeft)[2] - im.at<Vec3b>(i,nextRight)[2];
-			M.at<int>(i,j) = sqrt(bi*bi + gi*gi + ri*ri + bj*bj + gj*gj + rj*rj);
-			maxima = max(maxima,M.at<int>(i,j));
-		}
-	}
-
-	if(toDisplay){
-		Mat energyimage(im.rows,im.cols, CV_8UC1);
-		maxima /= 255;
-		if(maxima==0) maxima=1;
-		for(int i = 1; i < im.rows-1; ++i){
-			for (int j = 1; j < im.cols-1; ++j){
-					energyimage.at<char>(i,j) = M.at<int>(i,j)/maxima;
-			}
-		}
-		imshow("energyInd",energyimage);
-		waitKey(1);
-	}
-
-	return M;
-}
-*/
 
 int min(int a, int b, int c){
 	if(a<b){
@@ -101,10 +34,9 @@ int max(int a, int b){
 	else return a;
 }
 
-void energyIndivisual(Mat im, bool toDisplay,Mat& M){
+void energyIndivisual(Mat im, bool toDisplay,vvi& M){
 
 	int maxima = 0;
-	puts("energyIndivisual");
 	for (int i = 1; i < im.rows-1; ++i){
 		for (int j = 1; j < im.cols-1; ++j){
 			int bi = im.at<Vec3b>(i-1,j)[0] - im.at<Vec3b>(i+1,j)[0];
@@ -113,53 +45,48 @@ void energyIndivisual(Mat im, bool toDisplay,Mat& M){
 			int bj = im.at<Vec3b>(i,j-1)[0] - im.at<Vec3b>(i,j+1)[0];
 			int gj = im.at<Vec3b>(i,j-1)[1] - im.at<Vec3b>(i,j+1)[1];
 			int rj = im.at<Vec3b>(i,j-1)[2] - im.at<Vec3b>(i,j+1)[2];
-			M.at<int>(i,j) = int(sqrt(bi*bi + gi*gi + ri*ri + bj*bj + gj*gj + rj*rj));
-			maxima = max(maxima,M.at<int>(i,j));
+			M[i][j] = int(sqrt(bi*bi + gi*gi + ri*ri + bj*bj + gj*gj + rj*rj));
+			maxima = max(maxima,M[i][j]);
 		}
 	}
 
 	if(toDisplay){
-		Mat energyimage(im.rows,im.cols, CV_8UC1);
+		Mat energyimage = Mat::zeros(Size(im.cols,im.rows), CV_8UC1);
 		maxima /= 255;
 		if(maxima==0) maxima=1;
 		for(int i = 1; i < im.rows-1; ++i){
 			for (int j = 1; j < im.cols-1; ++j){
-					energyimage.at<char>(i,j) = M.at<int>(i,j)/maxima;
+					energyimage.at<uchar>(i,j) = M[i][j]/maxima;
 			}
 		}
 		imshow("energyInd",energyimage);
 		waitKey(1);
 	}
-	puts("energyIndivisual end");
 }
 
-void energyCumVer(Mat energyInd, string name, bool displayTranspose, bool toDisplay,Mat& M){
-	cout << "energyCumVer" << M.size() << endl;
+void energyCumVer(vvi& energyInd, string name, bool displayTranspose, bool toDisplay,vvi& M){
 	int maxima = 0;
+	for(int j = 1; j < energyInd[0].size()-1; ++j )
+		M[0][j] = 0;
 
-	cout <<"start"<<endl;
-
-	for(int j = 1; j < energyInd.cols-1; ++j )
-		M.at<int>(0,j) = 0;
-
-	for (int i = 1; i < energyInd.rows-1; ++i){
-		M.at<int>(i,1) = energyInd.at<int>(i,1) + min(M.at<int>(i-1,1),M.at<int>(i-1,1),M.at<int>(i-1,2));
-		maxima = max(maxima,M.at<int>(i,1));
-		M.at<int>(i, energyInd.cols-2) = energyInd.at<int>(i, energyInd.cols-2) + min(M.at<int>(i-1, energyInd.cols-3),M.at<int>(i-1, energyInd.cols-2),M.at<int>(i-1, energyInd.cols-2));
-		maxima = max(maxima,M.at<int>(i, energyInd.cols-2));
-		for (int j = 2; j < energyInd.cols-2; ++j){
-			M.at<int>(i,j) = energyInd.at<int>(i,j) + min(M.at<int>(i-1,j-1),M.at<int>(i-1,j),M.at<int>(i-1,j+1));
-			maxima = max(maxima,M.at<int>(i,j));
+	for (int i = 1; i < energyInd.size()-1; ++i){
+		M[i][1] = energyInd[i][1] + min(M[i-1][1],M[i-1][1],M[i-1][2]);
+		maxima = max(maxima,M[i][1]);
+		M[i][energyInd[0].size()-2] = energyInd[i][energyInd[0].size()-2] + min(M[i-1][energyInd[0].size()-3],M[i-1][energyInd[0].size()-2],M[i-1][energyInd[0].size()-2]);
+		maxima = max(maxima,M[i][energyInd[0].size()-2]);
+		for (int j = 2; j < energyInd[0].size()-2; ++j){
+			M[i][j] = energyInd[i][j] + min(M[i-1][j-1],M[i-1][j],M[i-1][j+1]);
+			maxima = max(maxima,M[i][j]);
 		}
 	}
 
 	if(toDisplay){
-		Mat cumEnergy(energyInd.rows,energyInd.cols, CV_8UC1); // just for display
+		Mat cumEnergy(energyInd.size(),energyInd[0].size(), CV_8UC1); // just for display
 		maxima/=255;
 		if(maxima==0) maxima=1;
-		for(int i = 1; i < energyInd.rows-1; ++i){
-			for (int j = 1; j < energyInd.cols-1; ++j){
-					cumEnergy.at<char>(i,j) = M.at<int>(i,j)/maxima;
+		for(int i = 1; i < energyInd.size()-1; ++i){
+			for (int j = 1; j < energyInd[0].size()-1; ++j){
+					cumEnergy.at<uchar>(i,j) = M[i][j]/maxima;
 			}
 		}
 		if(displayTranspose)
@@ -167,19 +94,18 @@ void energyCumVer(Mat energyInd, string name, bool displayTranspose, bool toDisp
 		else
 			imshow(name.c_str(),cumEnergy);	
 	}
-	cout <<"end"<<endl;
-
 }
 
-Mat indexMat(Mat m){
-	Mat ret(m.rows,m.cols, CV_32SC1);
+vvi indexMat(Mat m){
+	// Mat ret = Mat::zeros(Size(m.cols,m.rows), CV_32SC1);
+	vvi ret(m.rows,vi(m.cols,0));
 	for(int i = 0; i < m.rows; ++i){
-		ret.at<int>(i,0) = 0;
-		int counter = 1;
-		for(int j = 1; j < m.cols; ++j){
-			if(m.at<char>(i,j)!=255){
-				ret.at<int>(i,counter) = j;
-				++counter;
+		int cnt = 0;
+		for(int j = 0; j < m.cols; ++j){
+			if(m.at<uchar>(i,j)!=255){
+				// ret.at<int>(i,cnt) = j;
+				ret[i][cnt]=j;
+				++cnt;
 			}
 		}
 	}
@@ -187,25 +113,25 @@ Mat indexMat(Mat m){
 }
 
 void mergeSeams(Mat m1, Mat m2){
-	Mat cum = indexMat(m1);
+	vvi cum = indexMat(m1);
 	for(int i = 0; i < m2.rows; ++i){
 		for(int j = 0; j < m2.cols; ++j){
-			if(m2.at<char>(i,j)==255){
-				m1.at<char>(i,cum.at<int>(i,j)) = 255;
+			if(m2.at<uchar>(i,j)==255){
+				m1.at<uchar>(i,cum[i][j]) = 255;
 			}
 		}
 	}
 }
 
 Mat removeVerSeams(Mat im, Mat seams, int numSeams){
-	Mat ret(im.rows,im.cols-numSeams,CV_8UC3);
+	Mat ret = Mat::zeros(Size(im.cols-numSeams,im.rows),CV_8UC3);
 
 	for(int i = 0; i < im.rows; ++i){
-		int counter = 0;
+		int cnt = 0;
 		for(int j = 0; j < im.cols; ++j){
-			if(seams.at<char>(i,j)!=255){
-				ret.at<Vec3b>(i,counter) = im.at<Vec3b>(i,j);
-				++counter;
+			if(seams.at<uchar>(i,j)!=255){
+				ret.at<Vec3b>(i,cnt) = im.at<Vec3b>(i,j);
+				++cnt;
 			}
 		}
 	}
@@ -214,61 +140,57 @@ Mat removeVerSeams(Mat im, Mat seams, int numSeams){
 
 Mat markSeamVer(Mat im, int numSeams){
 
-	Mat ret(im.rows,im.cols,CV_8UC1);
+	Mat ret = Mat::zeros(Size(im.cols,im.rows),CV_8UC1);
 
-	for (int cnt = 0; cnt < numSeams; ++cnt,cout<<cnt<<endl){
-		cout<<cnt<<" " <<numSeams<<endl;
+	for (int cnt = 0; cnt < numSeams; ++cnt){
 
 		Mat smaller = removeVerSeams(im,ret,cnt);
-		puts("Allocating..");
-		Mat energyInd = Mat::zeros(Size(smaller.cols,smaller.rows),CV_32SC1);
-		puts("done..");
-		energyIndivisual(smaller,false,energyInd);
-		puts("Allocating..");
-		Mat energyCum = Mat::zeros(Size(energyInd.cols,energyInd.rows), CV_32SC1);
-		puts("Done");
-		energyCumVer(energyInd,"energyCum",false,false,energyCum);
-		puts("Allocating..");
-		Mat retLocal(im.rows,im.cols-cnt,CV_8UC1);
-		puts("Done.");
+		// Mat energyInd = Mat::zeros(Size(smaller.cols,smaller.rows),CV_32SC1);
+		vvi energyInd(smaller.rows,vi(smaller.cols,0));
+		energyIndivisual(smaller,true,energyInd);
+		// Mat energyCum = Mat::zeros(Size(energyInd[0].size(),energyInd.rows), CV_32SC1);
+		vvi energyCum(smaller.rows,vi(smaller.cols,0));
+		energyCumVer(energyInd,"energyCum",false,true,energyCum);
+		Mat retLocal = Mat::zeros(Size(smaller.cols,smaller.rows),CV_8UC1);
 
 		int min = INT_MAX;
 		int minInd = 0;
 		for(int j = 1; j < smaller.cols-1; ++j){
-			if(min>energyCum.at<int>(smaller.rows-2,j)){
-				min = energyCum.at<int>(smaller.rows-2,j);
+			if(min>energyCum[smaller.rows-2][j]){
+				min = energyCum[smaller.rows-2][j];
 				minInd = j;
 			}
 		}
-		puts("this");
+
+		waitKey(1);
 		Point curr(smaller.rows-2,minInd);
 
-		retLocal.at<char>(curr.x,curr.y) = 255;
+		retLocal.at<uchar>(curr.x,curr.y) = 255;
+		retLocal.at<uchar>(curr.x+1,curr.y) = 255;
 
-		for (int i = energyCum.rows-3; i >= 1; --i){
+		for (int i = energyCum.size()-3; i >= 0; --i){
 			int j = curr.y;
 			curr.x = i;
 			int min = INT_MAX;
 			if(j!=1){
-				if(energyCum.at<int>(i,j-1) < min){
-					min = energyCum.at<int>(i,j-1);
+				if(energyCum[i][j-1] < min){
+					min = energyCum[i][j-1];
 					curr.y = j-1;	
 				}
 			}
-			if(j!=energyCum.cols-2){
-				if(energyCum.at<int>(i,j+1) < min){
-					min = energyCum.at<int>(i,j+1);
+			if(j!=energyCum[0].size()-2){
+				if(energyCum[i][j+1] < min){
+					min = energyCum[i][j+1];
 					curr.y = j+1;
 				}
 			}
-			if(energyCum.at<int>(i,j) < min)
+			if(energyCum[i][j] < min)
 				curr.y = j;
 
-			retLocal.at<char>(curr.x,curr.y) = 255;
+			retLocal.at<uchar>(curr.x,curr.y) = 255;
 		}
-		puts("calling mergeSeams");
+
 		mergeSeams(ret,retLocal);
-		puts("returned");
 	}
 	
 	return ret;
@@ -280,25 +202,20 @@ void seam(Mat im, Mat oldSeamsHor, Mat oldSeamsVer, int numOldSeamsHor, int numO
 	Mat newIm2 = removeVerSeams(im,oldSeamsVer,numOldSeamsVer);
 
 	Mat newSeamsHor = markSeamVer(newIm1, numNewSeamsHor);
+	Mat newSeamsVer = markSeamVer(newIm2, numNewSeamsVer);
 
-// 	Mat newSeamsVer = markSeamVer(newIm2, numNewSeamsVer);
-// cout << "asdas" << endl;
+	imshow("new1",newSeamsHor);
+	imshow("new2",newSeamsVer);
 
-// 	// imshow("new1",newSeamsHor);
-// 	// imshow("new2",newSeamsVer);
-
-// 	mergeSeams(oldSeamsHor, newSeamsHor);
-// 	mergeSeams(oldSeamsVer, newSeamsVer);
-// cout << "asdas" << endl;
-
+	mergeSeams(oldSeamsHor, newSeamsHor);
+	mergeSeams(oldSeamsVer, newSeamsVer);
 }
 
 void showSeams(Mat im, Mat seamsHor, Mat seamsVer){
-	seamsHor = seamsHor.t();
 	Mat displayIm = im.clone();
 	for(int i = 0; i < displayIm.rows; ++i){
 		for(int j = 0; j < displayIm.cols; ++j){
-			if(seamsHor.at<char>(i,j)==255 || seamsVer.at<char>(i,j)==255){
+			if(seamsHor.at<uchar>(j,i)==255 || seamsVer.at<uchar>(i,j)==255){
 				displayIm.at<Vec3b>(i,j) = Vec3b(0,0,255);
 			}
 		}
@@ -308,58 +225,94 @@ void showSeams(Mat im, Mat seamsHor, Mat seamsVer){
 }
 
 Mat removeBothSeamsVerFirst(Mat im, Mat seamsHor, Mat seamsVer, int numSeamsHor, int numSeamsVer){
-	Mat ret(im.rows,im.cols-numSeamsVer,CV_8UC3);
-	Mat newSeamsHor(im.rows,im.cols-numSeamsVer,CV_8UC1);
+	Mat ret = Mat::zeros(Size(im.cols-numSeamsVer,im.rows),CV_8UC3);
+	Mat newSeamsHor = Mat::zeros(Size(im.cols-numSeamsVer,im.rows),CV_8UC1);
+
+	
+	for(int j = 0; j < im.cols; ++j){
+		int c=0;
+		for(int i = 0; i < im.rows; ++i){
+			if(seamsHor.at<uchar>(i,j)==255){
+				c++;
+			}
+		}
+		if(c!=numSeamsHor)
+			cout << "shouldn't hor" << endl;
+	}
 
 	for(int i = 0; i < im.rows; ++i){
-		int counter = 0;
+		int c=0;
 		for(int j = 0; j < im.cols; ++j){
-			if(seamsVer.at<char>(i,j)!=255){
-				ret.at<Vec3b>(i,counter) = im.at<Vec3b>(i,j);
-				newSeamsHor.at<char>(i,counter) = seamsHor.at<char>(i,j);
-				++counter;
+			if(seamsVer.at<uchar>(i,j)==255){
+				c++;
 			}
 		}
+		if(c!=numSeamsVer)
+			cout << "shouldn't ver" << endl;
 	}
 
-	Mat retFinal(im.rows-numSeamsHor,im.cols-numSeamsVer,CV_8UC3);
+	for(int i = 0; i < im.rows; ++i){
+		int cnt = 0;
+		for(int j = 0; j < im.cols; ++j){
+			if(seamsVer.at<uchar>(i,j)!=255){
+				ret.at<Vec3b>(i,cnt) = im.at<Vec3b>(i,j);
+				newSeamsHor.at<uchar>(i,cnt) = seamsHor.at<uchar>(i,j);
+				++cnt;
+			}
+		}
+		if(im.cols-cnt!=numSeamsVer)
+			cout << "Error!" << endl;
+	}
 
+	imshow("newSeamsHor",newSeamsHor);
+	imshow("oldSeamsHor",seamsHor);
+
+	waitKey(1);
+// cout << "done" << endl;
+
+	Mat retFinal = Mat::zeros(Size(im.cols-numSeamsVer,im.rows-numSeamsHor),CV_8UC3);
+/*
 	for(int j = 0; j < ret.cols; ++j){
-		int counter = 0;
+		int cnt = 0;
 		for(int i = 0; i < ret.rows; ++i){
-			if(newSeamsHor.at<char>(i,j)!=255){
-				retFinal.at<Vec3b>(counter,j) = ret.at<Vec3b>(i,j);
-				++counter;
+			if(newSeamsHor.at<uchar>(i,j)!=255){
+				retFinal.at<Vec3b>(cnt,j) = ret.at<Vec3b>(i,j);
+				++cnt;
 			}
 		}
+		if(ret.rows-cnt!=numSeamsHor){
+			cout <<"here " << (ret.rows-cnt)-numSeamsHor << endl;
+			cout << j  << endl;
+		}
 	}
-
+	*/
+// cout << "done" << endl;
 	return retFinal;
 }
 
 Mat removeBothSeamsHorFirst(Mat im, Mat seamsHor, Mat seamsVer, int numSeamsHor, int numSeamsVer){
-	Mat ret(im.rows-numSeamsHor,im.cols,CV_8UC3);
-	Mat newSeamsVer(im.rows-numSeamsHor,im.cols,CV_8UC1);
+	Mat ret = Mat::zeros(Size(im.cols,im.rows-numSeamsHor),CV_8UC3);
+	Mat newSeamsVer = Mat::zeros(Size(im.cols,im.rows-numSeamsHor),CV_8UC1);
 
 	for(int j = 0; j < im.cols; ++j){
-		int counter = 0;
+		int cnt = 0;
 		for(int i = 0; i < im.rows; ++i){
-			if(seamsHor.at<char>(i,j)!=255){
-				ret.at<Vec3b>(counter,j) = im.at<Vec3b>(i,j);
-				newSeamsVer.at<char>(counter,j) = seamsVer.at<char>(i,j);
-				++counter;
+			if(seamsHor.at<uchar>(i,j)!=255){
+				ret.at<Vec3b>(cnt,j) = im.at<Vec3b>(i,j);
+				newSeamsVer.at<uchar>(cnt,j) = seamsVer.at<uchar>(i,j);
+				++cnt;
 			}
 		}
 	}
 
-	Mat retFinal(im.rows-numSeamsHor,im.cols-numSeamsVer,CV_8UC3);
+	Mat retFinal = Mat::zeros(Size(im.cols-numSeamsVer,im.rows-numSeamsHor),CV_8UC3);
 
 	for(int i = 0; i < ret.rows; ++i){
-		int counter = 0;
+		int cnt = 0;
 		for(int j = 0; j < ret.cols; ++j){
-			if(newSeamsVer.at<char>(i,j)!=255){
-				retFinal.at<Vec3b>(i,counter) = ret.at<Vec3b>(i,j);
-				++counter;
+			if(newSeamsVer.at<uchar>(i,j)!=255){
+				retFinal.at<Vec3b>(i,cnt) = ret.at<Vec3b>(i,j);
+				++cnt;
 			}
 		}
 	}
@@ -369,10 +322,11 @@ Mat removeBothSeamsHorFirst(Mat im, Mat seamsHor, Mat seamsVer, int numSeamsHor,
 
 int main(int argc, char** argv){
 	Mat im = imread(argv[1]);
-	Mat empty1(im.rows,im.cols,CV_8UC1,0);
-	Mat empty2(im.rows,im.cols,CV_8UC1,0);
-	seam(im,empty1,empty2,0,0,10,10);
+	Mat empty1 = Mat::zeros(Size(im.rows,im.cols),CV_8UC1);
+	Mat empty2 = Mat::zeros(Size(im.cols,im.rows),CV_8UC1);
+	seam(im,empty1,empty2,0,0,atoi(argv[2]),atoi(argv[3]));
 	showSeams(im,empty1,empty2);
-	// imshow("compressed",removeBothSeamsVerFirst(im,empty1,empty2,10,10));
+	imshow("orig",im);
+	imshow("compressed",removeBothSeamsVerFirst(im,empty1.t(),empty2,atoi(argv[2]),atoi(argv[3])));
 	waitKey(0);
 }
